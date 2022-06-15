cbf4a93ab6bd5a3d7cf49b8b17ef667b4803fdf1
daniel-z-kaplan <48258016+daniel-z-kaplan@users.noreply.github.com>
2022-06-14 22:39:10 -0400

    Update train.py

--- Deceive/train.py
+++ Deceive/train.py
@@ -407,7 +407,8 @@ def subprocess_fn(rank, args, temp_dir):
             torch.distributed.init_process_group(backend='nccl', init_method=init_method, rank=rank, world_size=args.num_gpus)
 
     # Init torch_utils.
-    sync_device = torch.device('cuda', rank) if args.num_gpus > 1 else None
+    import torch_xla.core.xla_model as xm
+    sync_device = xm.xla_device() if args.num_gpus > 1 else None
     training_stats.init_multiprocessing(rank=rank, sync_device=sync_device)
     if rank != 0:
         custom_ops.verbosity = 'none'
